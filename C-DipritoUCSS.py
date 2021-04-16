107ba9f976552d2a512ba76463f5cac4074f5a39
gachipanaq <gachipanaq@gmail.com>
2021-04-16 16:06:56 -0500

    Machine Learning

--- C-DipritoUCSS.py
+++ C-DipritoUCSS.py
@@ -1,14 +1,12 @@
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
-stemmer = LancasterStemmer
+stemmer = LancasterStemmer()
 import numpy
 import tflearn
 import tensorflow
 import json
 import random
 import pickle
-
-#nltk.download('punkt')
 
 with open("contents.json") as archivo:
 	datos = json.load(archivo)
@@ -28,7 +26,27 @@ for contents in datos["contents"]:
 		if contents["tag"] not in tags:
 			tags.append(contents["tag"])
 
-print(palabras)
-print(auxX)
-print(auxY)
-print(tags)
+palabras = [stemmer.stem(w.lower()) for w in palabras if w!= "?"]
+palabras = sorted(list(set(palabras)))
+tags = sorted(tags)
+
+entrenamiento = []
+salida = []
+
+salidaVacia = [0 for _ in range(len(tags))]
+
+for x, documento in enumerate(auxX):
+	cubeta = []
+	auxPalabra = [stemmer.stem(w.lower()) for w in documento]
+	for w in palabras:
+		if w in auxPalabra:
+			cubeta.append(1)
+		else:
+			cubeta.append(0)
+	filaSalida = salidaVacia[:]
+	filaSalida[tags.index(auxY[x])] = 1
+	entrenamiento.append(cubeta)
+	salida.append(filaSalida)
+
+print(entrenamiento)
+print(salida)
