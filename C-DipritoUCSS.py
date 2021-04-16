4c5bea4d1e85fb753bc15342196648e37c8a4070
gachipanaq <gachipanaq@gmail.com>
2021-04-16 15:30:45 -0500

    bot

---
+++ C-DipritoUCSS.py
@@ -0,0 +1,11 @@
+import nltk
+form nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer
+import numpy
+import tflearn
+import tensorflow
+import json
+import random
+import pickle
+
+nltk.download('punkt')
